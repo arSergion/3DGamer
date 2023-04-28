93ede0acdea444291f175ea183e8f6341f9e4020
arSergion <detpul.rulit@gmail.com>
2023-04-29 02:22:36 +0300

    3DGamer
    3DGamer

--- 3DGamer/Assets/Scripts/CameraControler1.cs
+++ 3DGamer/Assets/Scripts/CameraControler1.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 public class CameraControler1 : MonoBehaviour
 {
     public float moveSpeed = 10.0f;
-    public float rotateSpeed = 10.0f;
     public float zoomSpeed = 100.0f;
 
     private Vector3 lastMousePosition;
@@ -23,13 +22,6 @@ public class CameraControler1 : MonoBehaviour
             Vector3 delta = new Vector3(mouseX - lastMousePosition.x, mouseY - lastMousePosition.y, 0);
             transform.Translate(-delta * Time.deltaTime * moveSpeed);
             lastMousePosition = new Vector3(mouseX, mouseY, 0);
-        }
-        else if (Input.GetMouseButton(1))
-        {
-            float rotateX = Input.GetAxis("Mouse X");
-            float rotateY = Input.GetAxis("Mouse Y");
-            transform.RotateAround(transform.position, Vector3.up, rotateX * rotateSpeed);
-            transform.RotateAround(transform.position, transform.right, -rotateY * rotateSpeed);
         }
 
         transform.position += transform.forward * mouseScroll * zoomSpeed * Time.deltaTime;
--- 3DGamer/Assets/Scripts/PlayerObject.cs
+++ 3DGamer/Assets/Scripts/PlayerObject.cs
@@ -3,12 +3,21 @@ using UnityEngine;
 public class PlayerObject : MonoBehaviour
 {
     public LayerMask layer;
-    private void Update(){
+    public float rotateSpeed = 60f;
+    private void PositionObject(){
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
     RaycastHit hit;
     if(Physics.Raycast(ray, out hit, 1000f, layer))
         transform.position = hit.point;
+    }
+    private void Start() {
+        PositionObject();
+    }
+    private void Update(){
+        PositionObject();
     if(Input.GetMouseButtonDown(0))
         Destroy(gameObject.GetComponent<PlayerObject>());
+    if(Input.GetKey(KeyCode.LeftShift))
+        transform.Rotate(Vector3.up * Time.deltaTime * rotateSpeed);
     }
 }
